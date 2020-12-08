14238ac37027c3ea98f054ab43687b4ef4ca4075
Andrew J. Thom <andrewt@aafoods.com>
2020-12-08 13:26:38 -0600

    more updates

--- wayback-machine/settings.gradle.kts
+++ wayback-machine/settings.gradle.kts
@@ -1,3 +1,3 @@
 
-rootProject.name = "wayback-machine"
+rootProject.name = "mn-covid-history"
 
--- wayback-machine/src/main/kotlin/dev/ajthom90/wayback/main.kt
+++ wayback-machine/src/main/kotlin/dev/ajthom90/wayback/main.kt
@@ -20,7 +20,7 @@ fun main(args: Array<String>) {
 	var date = LocalDate.of(2020, Month.MARCH, 4).minusDays(1L)
 	while (date < LocalDate.now()) {
 		val dateString = date.atTime(6, 0, 0).format(ymdFormatter)
-		val htmlPath = Paths.get("/Users/ajthom90/Desktop/SitUpdates/${date.format(ymdFormatter)}.html")
+		val htmlPath = Paths.get("../${date.format(ymdFormatter)}.html")
 		val html = if (Files.exists(htmlPath)) {
 			Files.readAllBytes(htmlPath).decodeToString()
 		} else {
@@ -34,7 +34,17 @@ fun main(args: Array<String>) {
 		}
 		val doc = Jsoup.parse(html)
 		val updatedDate = doc.select("strong").firstOrNull { it.text().contains("Updated ", ignoreCase = true) || it.text().contains("As of ", ignoreCase = true) } ?: date.format(ymdFormatter)
-		val mapTable = doc.select("div#map").select("table").firstOrNull()
+		val mapTable = if (date >= LocalDate.of(2020, Month.APRIL, 10)) {
+			doc.select("div#map").select("table").firstOrNull()
+		} else if (date >= LocalDate.of(2020, Month.APRIL, 5)) {
+			doc.select("table").first {
+				it.select("tr").first().text().contains("county", ignoreCase = true)
+			}
+		} else if (date < LocalDate.of(2020, Month.APRIL, 5)) {
+			null
+		} else {
+			null
+		}
 		if (mapTable != null) {
 			val bec = mapTable.select("tr").firstOrNull { it.text().contains(args[0], ignoreCase = true) }
 			if (bec != null) {
@@ -42,10 +52,14 @@ fun main(args: Array<String>) {
 					val totalCases = bec.select("td")[3].text().replace(",", "").toInt()
 					val deaths = bec.select("td")[4].text().replace(",", "").toInt()
 					totalCases to deaths
-				} else {
+				} else if (date < LocalDate.of(2020, Month.OCTOBER, 14) && date >= LocalDate.of(2020, Month.APRIL, 6)) {
 					val totalCases = bec.select("td")[1].text().replace(",", "").toInt()
 					val deaths = bec.select("td")[2].text().replace(",", "").toInt()
 					totalCases to deaths
+				} else if (date <= LocalDate.of(2020, Month.APRIL, 5)) {
+					bec.select("td")[1].text().replace(",", "").toInt() to 0
+				} else {
+					0 to 0
 				}
 				println("$updatedDate: $totalCases; $deaths")
 			}
@@ -69,3 +83,5 @@ class Closest(val available: Boolean, val url: String, val timestamp: String, va
 }
 
 data class Result(val dateString: String, val becCases: Int, val becDeaths: Int)
+
+
